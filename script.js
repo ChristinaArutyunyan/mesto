b8a1941af445f60287732f48549440f916670485
Christina Arutyunyan <kristinaharutyunyan77@gmaill.com>
2022-05-31 18:23:09 +0300

    первая попытка

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-var popup = document.querySelector('.profile__edit-form');
-var overlay = document.querySelector('.overlay')
-var popupOpenButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector(".profile__edit-form");
+const overlay = document.querySelector(".overlay");
+const form = document.querySelector(".profile__edit-form");
+const profileNameInput = document.querySelector(".profile__edit-form_name");
+const profileNameInformationInput = document.querySelector(
+  ".profile__edit-form_extra-information"
+);
+const popupOpenButton = document.querySelector(".profile__edit-button");
+const popupCloseButton = document.querySelector(".profile__close-button");
 
-popupOpenButton.addEventListener ('click', function() {
-    overlay.classList.add('overlay--show');
-})
+const profileName = document.querySelector(".profile__name");
+const profileNameInformation = document.querySelector(
+  ".profile__name-information"
+);
 
-// popup.classList.add('profile__edit-form--show');//
+popupOpenButton.addEventListener("click", function () {
+  overlay.classList.add("overlay--show");
+});
+
+popupCloseButton.addEventListener("click", function () {
+  profileNameInput.value = profileName.innerText;
+  profileNameInformationInput.value = profileNameInformation.innerText;
+  overlay.classList.remove("overlay--show");
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileName.innerText = profileNameInput.value;
+  profileNameInformation.innerText = profileNameInformationInput.value;
+  overlay.classList.remove("overlay--show");
+});
+
+document.querySelectorAll(".element__like").forEach(function (like) {
+  like.addEventListener("click", function () {
+    like.classList.toggle("element__like_active");
+  });
+});
